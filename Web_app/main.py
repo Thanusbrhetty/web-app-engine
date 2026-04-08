e7ff3a35de35f3f4c88aedbd8130dd402f8b4f80
Thanusbrhetty <thanubrshetty@gmail.com>
2026-04-08 14:39:50 +0530

    Create main.py

---
+++ Web_app/main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route(&#39;/&#39;)
+def home():
+# render_template automatically looks in the /templates folder
+return render_template(&#39;index.html&#39;, title=&quot;Home Page&quot;)
+
+if __name__ == &#39;__main__&#39;:
+# Local development server
+app.run(host=&#39;127.0.0.1&#39;, port=8080, debug=True)
